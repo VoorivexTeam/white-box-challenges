09d1ff1fc5f79add231b54a326dd3249f7728340
AmirMohammad Safari <amir@voorivex.team>
2026-03-25 01:20:58 +0330

    Add insane-csrf

---
+++ insane-csrf/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+const SIMPLE_TYPES = ['application/x-www-form-urlencoded', 'multipart/form-data', 'text/plain', ''];
+
+app.use(express.json({ type: () => true }));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  if (!req.cookies.name)
+    res.cookie('name', 'voorivex', { sameSite: 'None', secure: true });
+  next();
+});
+
+app.use((req, res, next) => {
+  const ct = (req.headers['content-type'] || '').split(';')[0].trim();
+  if (req.method === 'POST' && SIMPLE_TYPES.includes(ct))
+    return res.status(403).json({ error: 'Forbidden: simple content-type (CSRF)' });
+  next();
+});
+
+app.get('/profile',  (req, res) => res.json({ name: req.cookies.name || 'voorivex' }));
+app.post('/profile', (req, res) => {
+  res.cookie('name', req.body.name, { sameSite: 'None', secure: true });
+  res.json({ name: req.body.name });
+});
+
+app.listen(3000, () => console.log('Running on http://localhost:3000'));
